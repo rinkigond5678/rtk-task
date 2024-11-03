0e7ddada9462a220236b0b8fe6e1fcccc8b9ce3d
Rinki <rinkigond5678@gmail.com>
2024-11-03 16:17:33 +0530

    first

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { Container, Typography, Box } from "@mui/material";
+import AddHabitForm from "./components/add-habit-form";
+
+import HabitStats from "./components/habit-stats";
+import store from "./store/store";
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <Container maxWidth="md">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h2" component="h1" gutterBottom align="center">
+            Habit Tracker
+          </Typography>
+          <AddHabitForm />
+          
+          <HabitStats />
+        </Box>
+      </Container>
+    </Provider>
+  );
+};
+
+export default App;
+
